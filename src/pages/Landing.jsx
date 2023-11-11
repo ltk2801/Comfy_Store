ce3ec79306e76aade2e5e7b09b58ccb45cfb3161
ltk2801 <luutuankhanha3dt@gmail.com>
2023-11-11 21:18:57 +0700

    Complete Landing Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,22 @@
+import { Axios } from "../utils";
+import { FeaturedProducts, Hero } from "../components";
+
+const url = "/products?featured=true";
+
+export const loader = async () => {
+  const response = await Axios.get(url);
+  const products = response.data.data;
+
+  return { products };
+};
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  );
 };
 
 export default Landing;
